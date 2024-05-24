ae2ed77f1d125057831423f1b8853f926bf8b5ce
bammgy <98696898+bammgy@users.noreply.github.com>
2024-05-24 12:20:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {};
+    member.skills = [
+        {name: 'HTML', level: 2},
+        {name: 'CSS', level: 3},
+        {name: 'JavaScript', level: 1},
+    ];
+    return member;
+}
